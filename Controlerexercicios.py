ecfca709636b0c8adb66415df2957ecfe459b1ba
Ikauan <kauanmartins321@>
2022-05-05 19:47:11 -0300

    "Commit"

--- Controlerexercicios.py
+++ Controlerexercicios.py
@@ -3,6 +3,10 @@ import this
 
 opcao = 0
 
+a = 10
+b = 20
+c = 0
+
 bacalhau = 0
 
 base = 0
@@ -14,14 +18,15 @@ def Menu():
           '2. Ver número antecessor\n'
           '3. Mostrar a área de um retângulo\n'
           '4. Ver idade convertida\n'
-          '5.')
+          '5. Ver os eleitores\n'
+          '6. ')
     this.opcao = int(input())
 
 
 def Escolha():
     Menu() #Chamando o método menu, para mostra-lo
     if(this.opcao == 1):
-        print(Modelexercicios.Troca())
+        print(Modelexercicios.Troca(a, b))
     elif(this.opcao == 2):
         ColetarNum1()
         print(Modelexercicios.Sub(this.bacalhau))
@@ -30,10 +35,12 @@ def Escolha():
         Altura()
         print('A área do retângulo corresponde a: ')
         print( Modelexercicios.Retangulo(this.base, this.altura))
-    elif(this.opco == 4):
-        print(Modelexercicios.Idade())
+    elif(this.opcao == 4):
+        Idade()
+        print(Modelexercicios.Idade(this.idade, this.meses, this.dias))
     elif(this.opcao == 5):
-        print(Modelexercicios.Votos())
+        Eleitores()
+        print(Modelexercicios.Eleitores(this.eleitores, this.brancos, this.nulos, this.validos))
     else:
         print('Número invalido')
 
@@ -44,6 +51,20 @@ def ColetarNum1():
 def Base():
     print('Digite a base do triângulo: ')
     this.base = float(input())
+
 def Altura():
     print('Digite a altura do triângulo: ')
     this.altura = float(input())
+
+def Idade():
+    print('Digite sua idade: ')
+    this.idade = int(input())
+    this.meses = 30
+    this.dias = 365
+
+def Eleitores():
+    print('Quantos eleitores votaram em seu estado? ')
+    this.eleitores = int(input())
+    this.brancos = this.eleitores
+    this.nulos = this.eleitores
+    this.validos = this.eleitores
--- Modelexercicios.py
+++ Modelexercicios.py
@@ -1,12 +1,16 @@
 import this
 
-def Troca():
+def Troca(a, b):
+
     a = 10
     b = 20
-    print('Valor de A {} valor de B {}'.format(a, b))
     a, b = b, a
 
-    return 'Novo valor de A é: {}, novo valor de B é: {}'.format(a, b)
+    return 'A = {}  B = {}'.format(a, b)
+    #print('Valor de A {} valor de B {}'.format(a, b))
+    #a, b = b, a
+
+    #return 'Novo valor de A é: {}, novo valor de B é: {}'.format(a, b)
 
 def Sub(num1):
     return num1 -1
@@ -25,30 +29,28 @@ def Retangulo(base, altura):
 
     #return 'A área do retângulo corresponde a {}cm'.format(area)
 
-def Idade():
-    idade = int(input('Digite sua idade: '))
-    meses = idade * 12
+def Idade(idade, meses, dias):
+    meses = idade * 30
     dias = idade * 365
-    horas = idade * 8760
-    minutos = idade * 525600
+    return 'Você tem {}.\n' \
+           'Você tem {} em meses.\n' \
+           'Você tem {} em dias'.format(idade, meses, dias)
+    #idade = int(input('Digite sua idade: '))
+    #meses = idade * 12
+    #dias = idade * 365
+    #horas = idade * 8760
+    #minutos = idade * 525600
 
-    return 'Você tem {} anos\n' \
-           'Você tem {} de vida\n' \
-           'Você tem {} dias de vida\n' \
-           'Você tem {} horas de vida\n' \
-           'Você tem {} minutos de vida'.format(idade, meses, dias, horas, minutos)
+    #return 'Você tem {} anos\n' \
+           #'Você tem {} de vida\n' \
+           #'Você tem {} dias de vida\n'.format(idade, meses, dias) \
+           #'Você tem {} horas de vida\n' \
+           #'Você tem {} minutos de vida'.format(idade, meses, dias, horas, minutos)
 
-# def Votos:
-   # pessoas = int(input(1500))
-   # brancos = 0
-   # nulos = 0
-   # validos = 0
+def Eleitores(eleitores, brancos, nulos, validos): # Ajuda
+    eleitores = eleitores
+    brancos = eleitores % 100
+    nulos = (eleitores - brancos) % 100
+    validos = (eleitores - brancos - nulos ) % 100
 
-
-#def Teste(idade, meses, dias):
-#    this.idade = 0
-#    this.meses = idade
-#    this.dias = idade
-#
-#    return idade, meses * 12, dias * 365
-
+    return eleitores, brancos, nulos, validos
